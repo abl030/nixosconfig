bd8af2ff6cae37209f7e3347e41a2f3d5dafb081
abl030 <abl030@gmail.com>
2024-10-05 21:28:32 +0800

    cleaned out some old neovim plugins

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -9,24 +9,23 @@ local plugins = {
 		"tpope/vim-sensible",
 		lazy = false,
 	},
-	{
-		"preservim/nerdtree",
-		lazy = false,
-	},
+	-- {
+	-- 	"preservim/nerdtree",
+	-- 	lazy = false,
+	-- },
 	{
 		"tpope/vim-surround",
 		lazy = false,
 	},
-	{
-		"aymericbeaumet/vim-symlink",
-		lazy = false,
-	},
-	{
-		"moll/vim-bbye",
-		lazy = false,
-	},
-	{ "chrisbra/improvedft", lazy = false },
-	-- In order to modify the `lspconfig` configuration:
+	-- {
+	-- 	"aymericbeaumet/vim-symlink",
+	-- 	lazy = false,
+	-- },
+	-- {
+	-- 	"moll/vim-bbye",
+	-- 	lazy = false,
+	-- },
+	{ "backdround/improved-ft.nvim", event = { "BufRead", "BufNewFile" } },
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
