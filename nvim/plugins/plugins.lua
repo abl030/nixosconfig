02ebc4078a6979baa90f5cf21bfb35fbb060797c
abl030 <abl030@gmail.com>
2024-10-07 11:48:16 +0800

    added and configured improvedft

--- nvim/plugins/plugins.lua
+++ nvim/plugins/plugins.lua
@@ -6,13 +6,49 @@ local plugins = {
 		event = { "BufRead", "BufNewFile" },
 	},
 
-	{
-		"tpope/vim-surround",
-		event = { "BufRead", "BufNewFile" },
-	},
+	-- {
+	-- 	"tpope/vim-surround",
+	-- 	event = { "BufRead", "BufNewFile" },
+	-- },
 
 	{
 		"backdround/improved-ft.nvim",
+		opts = function()
+			return {
+				-- Maps default f/F/t/T/;/, keys.
+				-- default: false
+				use_default_mappings = false,
+				-- Ignores case of the given characters.
+				-- default: false
+				ignore_char_case = true,
+				-- Takes a last hop direction into account during repetition hops
+				-- default: false
+				use_relative_repetition = true,
+				-- Uses direction-relative offsets during repetition hops.
+				-- default: false
+				use_relative_repetition_offsets = true,
+			}
+		end,
+		config = function(_, opts)
+			local ft = require("improved-ft")
+			ft.setup(opts)
+
+			-- Key mapping function
+			local map = function(key, fn, description)
+				vim.keymap.set({ "n", "x", "o" }, key, fn, {
+					desc = description,
+					expr = true,
+				})
+			end
+
+			-- Key mappings
+			map("f", ft.hop_forward_to_char, "Hop forward to a given char")
+			map("F", ft.hop_backward_to_char, "Hop backward to a given char")
+			map("t", ft.hop_forward_to_pre_char, "Hop forward before a given char")
+			map("T", ft.hop_backward_to_pre_char, "Hop backward before a given char")
+			map(":", ft.repeat_forward, "Repeat hop forward to a last given char")
+			map(",", ft.repeat_backward, "Repeat hop backward to a last given char")
+		end,
 		event = { "BufRead", "BufNewFile" },
 	},
 
