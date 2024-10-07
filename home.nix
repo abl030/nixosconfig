2a5449275843aa0e495e6619cbd4e31d5ab04df7
abl030 <abl030@gmail.com>
2024-10-07 15:03:40 +0800

    cleaned up home manager directory structure

--- home.nix
+++ home.nix
@@ -20,8 +20,8 @@
   # home.sessionVariables.NIXD_FLAGS = "-log=error";
 
   imports = [
-    ./zsh/zsh.nix
-    ./nvim/nvim.nix
+    ./home/zsh/zsh.nix
+    ./home/nvim/nvim.nix
   ];
 
   # The home.packages option allows you to install Nix packages into your
