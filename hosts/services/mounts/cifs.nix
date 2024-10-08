bd1f2fc835b802ff2fcf87399ba22ce9f62c732b
abl030 <abl030@g.m.a.i.l>
2024-10-08 17:09:58 +0800

    we have our nfs module working well now. This only took like 5 hours.

---
+++ hosts/services/mounts/cifs.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  # For mount.cifs, required unless domain name resolution is not needed.
+  # environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/share" = {
+    device = "//192.168.1.2/data";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/home/asus/smb-secrets" ];
+  };
+}
