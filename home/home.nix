560832f4d163a962ed78de549a985c5c28a78f36
abl030 <abl030@gmail.com>
2024-10-09 19:51:40 +0800

    neofetch

--- home/home.nix
+++ home/home.nix
@@ -34,6 +34,7 @@
     pkgs.tmux
     pkgs.git
     pkgs.gh
+    pkgs.neofetch
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
