a9858a46d213aed254dff1a9b0f5bd3020e3c4be
abl030 <abl030@g.m.a.i.l>
2024-10-09 21:07:06 +0800

    sops and age in home. strangely we'll need them in both home and config

--- home/home.nix
+++ home/home.nix
@@ -35,6 +35,8 @@
     pkgs.git
     pkgs.gh
     pkgs.neofetch
+    pkgs.sops
+    pkgs.age
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
