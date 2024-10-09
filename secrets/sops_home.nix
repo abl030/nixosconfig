fffc8335b7d6d22c917c039a2fc1fd726cc624d8
abl030 <abl030@gmail.com>
2024-10-09 21:52:07 +0800

    added epi as host. smb cred test built but not tested.

---
+++ secrets/sops_home.nix
@@ -0,0 +1,25 @@
+{ inputs, ... }:
+
+let
+  username = builtins.getEnv "USER"; # Gets the current username
+  homeDir = "/home/${username}"; # Constructs the home directory path
+in
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  sops = {
+    age.keyFile = "${homeDir}/.config/sops/age/keys.txt"; # Use dynamic home directory
+    defaultSopsFile = ./secrets.yaml;
+    validateSopsFiles = false;
+
+    secrets = {
+      "smbpassword" = {
+        # Use dynamic username
+        path = "${homeDir}/smb_credential"; # Use dynamic home directory
+      };
+    };
+  };
+}
+
