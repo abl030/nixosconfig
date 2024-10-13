ed1734763deabc97924ea27ce12b96ad60d04cd3
abl030 <abl030@gmail.com>
2024-10-13 19:10:52 +0800

    well we've allowed for most edge cases of nfs now. always load the kernel module

--- hosts/services/mounts/nfs.nix
+++ hosts/services/mounts/nfs.nix
@@ -4,8 +4,14 @@
 # Here's the uraid NFS rules: 
 # 100.70.190.0/24(rw,sync,no_subtree_check,anonuid=99,anongid=100,all_squash) 192.168.1.0/24(sec=sys,root_squash,noaccess) 
 # The id stuff squashes all requests to nobody/users which is default unraid and just leads to less headaches.
-
+{ pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [ nfs-utils ];
+  boot.initrd = {
+    supportedFilesystems = [ "nfs" ];
+    kernelModules = [ "nfs" ];
+  };
+
   fileSystems."/mnt/data" = {
     device = "tower:/mnt/user/data/";
     fsType = "nfs";
