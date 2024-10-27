1e4a86af289275f39cc43f557f5d078df0f32354
asus <abl030@gmail.com>
2024-10-28 07:00:08 +0800

    fixed asus nix-flakes

--- hosts/asus/configuration.nix
+++ hosts/asus/configuration.nix
@@ -183,10 +183,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
