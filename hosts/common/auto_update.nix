4c97b633718e90de58086c94bee4fa93a21ea1d5
abl030 <abl030@gmail.com>
2024-11-19 08:12:06 +0800

    modularised our common config, passed through inputs into nixosconfig, auto-update is now common

---
+++ hosts/common/auto_update.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, inputs, ... }:
+{
+  # Enable auto-upgrade
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+}
--- hosts/epi/configuration.nix
+++ hosts/epi/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -16,6 +16,7 @@
       # ../services/mounts/cifs.nix
 
       # ../services/nvidia/nvidia.nix
+      ../common/configuration.nix
     ];
 
   # lets use the latest kernel because we are stupid
