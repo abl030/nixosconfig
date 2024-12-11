3a839aed8e5e78d6a0460dbed072edf9dde7ba35
abl030 <abl030@gmail.com>
2024-12-11 13:48:47 +0800

    tidied up gnome-remote-desktop and added in a polkit section as per the wiki. Still unsure about it though

---
+++ hosts/services/display/gnome-remote-desktop.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+
+  security.polkit.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function (action, subject) {
+      if (
+        subject.isInGroup("users") &&
+        [
+          "org.freedesktop.login1.reboot",
+          "org.freedesktop.login1.reboot-multiple-sessions",
+          "org.freedesktop.login1.power-off",
+          "org.freedesktop.login1.power-off-multiple-sessions",
+        ].indexOf(action.id) !== -1
+      ) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
+}
