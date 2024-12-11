ee438247f0fa2a6d16485a4afdf34a9df58db8cd
abl030 <abl030@gmail.com>
2024-12-11 13:18:58 +0800

    Little fixes, intel now works with vaapi, removed the 20 minute shutdown so sleep is now manual

--- home/utils/desktop.nix
+++ home/utils/desktop.nix
@@ -7,7 +7,10 @@
     pkgs.google-chrome
     pkgs.libreoffice-qt
     pkgs.remmina
+    #VLC doesn't user VAAPI anymore because devs are fighting over FFMPEG
+    #Just use gnome videos for now
     pkgs.vlc
+    pkgs.mpv
     pkgs.tailscale-systray
     pkgs.warp-terminal
     pkgs.alacritty
--- hosts/services/nvidia/intel.nix
+++ hosts/services/nvidia/intel.nix
@@ -1,14 +1,17 @@
 { pkgs, ... }:
 {
-  hardware.opengl = {
+  hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       # your Open GL, Vulkan and VAAPI drivers
-      vpl-gpu-rt # for newer GPUs on NixOS >24.05 or unstable
+      # vpl-gpu-rt # for newer GPUs on NixOS >24.05 or unstable
       # onevpl-intel-gpu  # for newer GPUs on NixOS <= 24.05
       intel-media-driver
       # intel-media-sdk   # for older GPUs
+      # libvdpau-va-gl
+      intel-vaapi-driver
     ];
   };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; }; # Optionally, set the environment variable
 }
 
