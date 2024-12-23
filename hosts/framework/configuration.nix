d120e781d5608580406882dec08bbc810b7c8c4c
abl030 <abl030@gmail.com>
2024-12-23 14:58:09 +0800

    added in our wifi regulatory domain

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -24,6 +24,26 @@
 
   # Framework specific hardware-configuration
   services.fwupd.enable = true;
+  services.fwupd.extraRemotes = [ "lvfs-testing" ];
+
+  # Make fingerprint reader work
+  services.fprintd.enable = true;
+
+  # Wifi fix 
+  hardware.wirelessRegulatoryDatabase = true;
+  boot.extraModprobeConfig = ''
+    options cfg80211 ieee80211_regdom="AU"
+  '';
+
+  # # we need fwupd 1.9.7 to downgrade the fingerprint sensor firmware
+  # services.fwupd.package = (import
+  #   (builtins.fetchTarball {
+  #     url = "https://github.com/NixOS/nixpkgs/archive/bb2009ca185d97813e75736c2b8d1d8bb81bde05.tar.gz";
+  #     sha256 = "sha256:003qcrsq5g5lggfrpq31gcvj82lb065xvr7bpfa8ddsw8x4dnysk";
+  #   })
+  #   {
+  #     inherit (pkgs) system;
+  #   }).fwupd;
 
   # Hardware acceleration for video
   hardware.opengl.enable = true;
@@ -129,6 +149,8 @@
     git
     vim
     gnome-remote-desktop
+    dmidecode
+    fprintd
   ];
 
   programs.zsh.enable = true;
