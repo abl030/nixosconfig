5ddafb9146e22eb09adba23e5de1012559bc29ec
abl030 <abl030@gmail.com>
2025-01-09 01:03:41 +0000

    win32yank in wsl (direct link though so may break - and won't work on different user installs)

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -6,5 +6,11 @@
     ../../home/utils/common.nix
     # ../../home/utils/invidious.nix
   ];
+
+  programs.zsh = {
+    initExtra = ''
+      export PATH="/mnt/c/Users/andy.b/AppData/Local/Microsoft/WinGet/Packages/equalsraf.win32yank_Microsoft.Winget.Source_8wekyb3d8bbwe:$PATH"
+    '';
+  };
 }
  
