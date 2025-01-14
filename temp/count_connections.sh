2aa0d9d281743d057e5bf8f34f5b403ca562113e
abl030 <abl030@gmail.com>
2025-01-14 22:24:46 +0800

    cleaned up temp files

--- temp/count_connections.sh
+++
@@ -1,7 +0,0 @@
-# Count regular SSH connections
-ssh_count=$(netstat -nt | awk '$4 ~ /:22$/ && $6 == "ESTABLISHED"' | wc -l)
-
-# Count Tailscale SSH connections
-ts_count=$(who | grep -c "pts/")
-
-echo $((ts_count))
