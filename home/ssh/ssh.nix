5127f6d6160119b6c203b0cb41b8221e2b27dcfe
abl030 <abl030@gmail.com>
2025-01-15 20:16:21 +0800

    started our ssh journey. config is now manager by nix

--- home/ssh/ssh.nix
+++ home/ssh/ssh.nix
@@ -5,6 +5,7 @@
 { config, inputs, home, pkgs, ... }:
 {
   home.file = {
-    ".ssh/authorized_keys".source = ./authorized_keys;
+    # ".ssh/authorized_keys".source = ./authorized_keys;
+    ".ssh/config".source = ./config;
   };
 }
