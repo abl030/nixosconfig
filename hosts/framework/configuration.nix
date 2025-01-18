0d132bdb08031e3f0152995912c54ca946aa33a9
abl030 <abl030@gmail.com>
2025-01-18 14:48:37 +0800

    framework is now framework.....

--- hosts/framework/configuration.nix
+++ hosts/framework/configuration.nix
@@ -65,7 +65,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "framework"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
