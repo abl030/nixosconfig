23307f1a600c85fab430ed83299b29972b7ecbcc
abl030 <abl030@gmail.com>
2025-01-21 01:24:05 +0000

    zsh is working quite nicely now

--- home/zsh/zsh.nix
+++ home/zsh/zsh.nix
@@ -17,12 +17,17 @@
   # programs.zsh.zprof.enable = true;
 
   programs.zsh = {
+    history = {
+      size = 10000;
+      save = 10000;
+    };
     initExtra = ''
 
       [[ ! -f ${config.home.homeDirectory}/.zshrc2 ]] || source ${config.home.homeDirectory}/.zshrc2
           source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
                 source ${pkgs.zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
                 zstyle ':completion:*:default' list-colors 'di=0;37'
+                zstyle ':autocomplete:history-search-backward:*' list-lines 2000
 '';
   };
 }
