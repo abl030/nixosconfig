29680d7d54aa8c5fef14192af9b1e3791a44115a
abl030 <abl030@gmail.com>
2025-04-18 22:07:11 +0800

    added in our zfs data mirror

--- hosts/proxmox-vm/configuration.nix
+++ hosts/proxmox-vm/configuration.nix
@@ -28,6 +28,18 @@
         # "gid=100" # users group in NixOS often
       ];
     };
+  fileSystems."/mnt/data2" = # Choose your desired mount point inside the VM
+    {
+      device = "data"; # <-- Use the tag you set in Proxmox
+      fsType = "virtiofs";
+      options = [
+        "defaults"
+        # Potentially add UID/GID mapping options here if needed, e.g.:
+        # "uid=1000"
+        # "gid=100" # users group in NixOS often
+      ];
+    };
+
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
