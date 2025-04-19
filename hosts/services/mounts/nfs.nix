bc87b2502728b626167a510cc18663b07862e745
abl030 <abl030@gmail.com>
2025-04-19 09:33:10 +0800

    added in our git ignore, hopefully it works, also immich is now moved to apollo. fun times

--- hosts/services/mounts/nfs.nix
+++ hosts/services/mounts/nfs.nix
@@ -33,6 +33,29 @@
       # Use NFS version 4.2
       "nfsvers=4.2"
     ];
+  };
+  fileSystems."/mnt/appdata" = {
+    device = "tower:/mnt/user/appdata/";
+    fsType = "nfs";
+    options = [
+      # We need this bit so that the mount works on tailscale. 
+      # Otherwise it will load at boot and tailscale isn't up yet.
+      # Automount when accessed
+      "x-systemd.automount"
+      # Do not mount at boot, only when needed
+      "noauto"
+      # Ensures the mount depends on Tailscale being up
+      "_netdev"
+      # Requires Tailscale service to be active before mounting
+      "x-systemd.requires=tailscaled.service"
+      # Unmount after 300 seconds of inactivity
+      "x-systemd.idle-timeout=300"
+      # Do not update file access times (improves performance)
+      "noatime"
+      # Use NFS version 4.2
+      "nfsvers=4.2"
+    ];
+
 
   };
 }
