6ba7cc6b1893861ddb5df6f9d85996313adef88d
abl030 <abl030@gmail.com>
2025-05-30 09:55:43 +0800

    enp9s0 doesn't exist anymore

--- hosts/epi/configuration.nix
+++ hosts/epi/configuration.nix
@@ -72,7 +72,7 @@
   networking.hostName = "epimetheus"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  networking.interfaces.enp9s0.wakeOnLan.enable = true;
+  # networking.interfaces.enp9s0.wakeOnLan.enable = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
