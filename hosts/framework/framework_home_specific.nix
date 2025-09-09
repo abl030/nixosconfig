a7c62efa8f90c16bd75cf72ed6c1568dae2ef7b0
abl030 <abl030@gmail.com>
2025-09-09 20:55:34 +0800

    Fixed up thumbnail packages in framework
    I dont even use this anymore. I am pretty sure in its current state it
    doesn't even work.

--- hosts/framework/framework_home_specific.nix
+++ hosts/framework/framework_home_specific.nix
@@ -6,8 +6,11 @@
     pkgs.kdePackages.dolphin
     # These are our thumbnailers. QT5 because we using LXQT.
     # Use the KDE ones is you are using KDE elsewhere
-    pkgs.libsForQt5.kdegraphics-thumbnailers
-    pkgs.libsForQt5.kio-extras
+    # pkgs.libsForQt5.kdegraphics-thumbnailers
+    # pkgs.libsForQt5.kdegraphics-thumbnailers
+    pkgs.kdePackages.kdegraphics-thumbnailers
+    # pkgs.libsForQt5.kio-extras
+    pkgs.kdePackages.kio-extras
     pkgs.kdePackages.ffmpegthumbs
     pkgs.kdePackages.dolphin-plugins
     pkgs.kdePackages.qtwayland
