592b294139f6f9efd76e3fdaeef01722e56f4b95
abl030 <abl030@gmail.com>
2025-09-10 07:54:40 +0000

    Moved WSL to Bash and update the config
    Zsh on WSL is stubbornly broken.
    Recursive loops.
    I have stripped it down to nothing and tried and still get the error. So
    I am at a loss. Created a bash recreation and now use that. Stripped
    down the home config of wsl too. The main config was causing NVIM load
    errors. It's to do with how home manager and how it interacts with WSL.
    Not worth pursuing anymore.

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -1,10 +1,20 @@
-{ ... }:
-{
-  imports = [
+{ config, pkgs, ... }:
 
-    ../../home/home.nix
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "nixos";
+  home.homeDirectory = "/home/nixos/";
+
+  imports = [
+    # ../../home/zsh/zsh2.nix
+    # ./fish/fish.nix
+    ../../home/bash/bash.nix
+    ../../home/nvim/nvim.nix
+    #It doesn't make sense to use home-manager for our authorized keys file. It's weird but
+    ../../home/ssh/ssh.nix
+    # ../secrets/sops_home.nix
     ../../home/utils/common.nix
-    # ../../home/utils/invidious.nix
   ];
 
   programs.zsh = {
@@ -12,5 +22,71 @@
       export PATH="/mnt/c/Users/andy.b/AppData/Local/Microsoft/WinGet/Packages/equalsraf.win32yank_Microsoft.Winget.Source_8wekyb3d8bbwe:$PATH"
     '';
   };
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/nixos/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
- 
