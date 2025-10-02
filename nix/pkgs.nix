10776755bec38b83abef18b90348692b8d1ef76a
abl030 <abl030@gmail.com>
2025-10-02 13:05:52 +0800

    Split out our devshell config to cleanup flake.nix
    Also added a linter helper function. Right now sort of doesn't work.

---
+++ nix/pkgs.nix
@@ -0,0 +1,15 @@
+# Bootstrap pkgs (with overlays) for all perSystem consumers (devShells, formatter, checks).
+# Rationale:
+# - flake-parts does not auto-init pkgs; we import nixpkgs here so everything
+#   inside perSystem sees the same overlayed package set as your NixOS/HM builds.
+
+{ inputs, ... }: {
+  perSystem = { system, ... }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      overlays = import ./overlay.nix { inherit inputs; };
+      config = { };
+    };
+  };
+}
+
