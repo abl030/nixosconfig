ff3cb2666a7ace7a7494f7af39f806749a9021a1
abl030 <abl030@gmail.com>
2025-10-06 11:49:55 +0800

    Further testing our actions

--- modules/nixos/github-runner.nix
+++ modules/nixos/github-runner.nix
@@ -48,7 +48,7 @@ in {
 
       # By not setting `workDir`, we use the module's default inside /var/lib, which it manages correctly.
       extraLabels = ["nix" "proxmox-vm"];
-      extraPackages = [pkgs.git pkgs.gnutar pkgs.xz pkgs.cacert pkgs.nix];
+      extraPackages = [pkgs.git pkgs.gnutar pkgs.xz pkgs.cacert pkgs.nix pkgs.cachix];
     };
 
     # Trust the group the module creates. The group name is derived from the service name.
