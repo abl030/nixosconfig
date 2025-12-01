4a960b77cbe473225c4e90518dd4dd618f723f07
nix <acme@ablz.au>
2025-12-01 13:34:48 +0800

    Added our kuma monitoring stack
    Now to add auto kuma

---
+++ docker/uptime-kuma/docker-compose.nix
@@ -0,0 +1,26 @@
+{config, ...}: {
+  systemd.services.uptime-kuma-stack = {
+    description = "Uptime Kuma Docker Compose Stack";
+    restartIfChanged = false;
+    reloadIfChanged = true;
+    requires = ["docker.service" "network-online.target"];
+    after = ["docker.service" "network-online.target"];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      Environment = "COMPOSE_PROJECT_NAME=uptime-kuma";
+
+      ExecStart = "${config.virtualisation.docker.package}/bin/docker compose -f ${./docker-compose.yml} up -d --remove-orphans";
+      ExecStop = "${config.virtualisation.docker.package}/bin/docker compose -f ${./docker-compose.yml} down";
+      ExecReload = "${config.virtualisation.docker.package}/bin/docker compose -f ${./docker-compose.yml} up -d --remove-orphans";
+
+      Restart = "on-failure";
+      RestartSec = "30s";
+      StandardOutput = "journal";
+      StandardError = "journal";
+    };
+
+    wantedBy = ["multi-user.target"];
+  };
+}
