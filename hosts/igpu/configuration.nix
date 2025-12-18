acabc11b0b3e940d2ad3f748fe962dc0e86939be
abl030 <abl030@gmail.com>
2025-12-18 13:00:35 +0800

    How long has igp been on UTC?

--- hosts/igpu/configuration.nix
+++ hosts/igpu/configuration.nix
@@ -76,7 +76,7 @@
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Australia/Perth";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
